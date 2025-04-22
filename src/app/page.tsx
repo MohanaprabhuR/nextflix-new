bc0e7168d85f9615d3485240820231bf2cd1c754
Mohanaprabhu.R <mohan@timeless.co>
2025-04-22 11:23:02 +0530

    Home page initaldata set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import HomeClient from "./page-client";
 
 export default async function Home() {
   const [showsResponse, genresResponse] = await Promise.all([
-    fetch(`${process.env.API_URL}/api/shows?populate=*`, {}),
-    fetch(`${process.env.API_URL}/api/genres?populate=*`, {}),
+    fetch(`${process.env.API_URL}/api/heroCarousal?populate=*`, {}),
+    fetch(`${process.env.API_URL}/api/swimlaneCarousel?populate=*`, {}),
   ]);
 
   const [shows, genres] = await Promise.all([
